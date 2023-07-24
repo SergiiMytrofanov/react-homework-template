068a5509205a8555a39b24aa469e726aea4545ce
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-07-24 21:05:54 +0300

    counter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleIncrement = (evt) => {
+    // Отримуємо поточний стан count
+    const { count } = this.state;
+    // Отримуємо значення кроку з пропів
+    const { step } = this.props;
+    // Оновлюємо стан, збільшуючи count на значення кроку
+    this.setState({ count: count + step });
+  };
+
+  handleDecrement = (evt) => {
+    // Отримуємо поточний стан count
+    const { count } = this.state;
+    // Отримуємо значення кроку з пропів
+    const { step } = this.props;
+    // Оновлюємо стан, зменшуючи count на значення кроку
+    this.setState({ count: count - step });
+  };
+
+  render() {
+    // Отримуємо поточний стан count
+    const { count } = this.state;
+    // Отримуємо значення кроку з пропів
+    const { step } = this.props;
+
+    return (
+      <div>
+        <span>{count}</span>
+        <button type="button" onClick={this.handleIncrement}>
+          Increment by {step}
+        </button>
+        <button type="button" onClick={this.handleDecrement}>
+          Decrement by {step}
+        </button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Counter step={5} />, document.getElementById("root"));
