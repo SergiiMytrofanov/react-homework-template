6eb1aba77e2faafc1d6fa8256a904eae6ae1395f
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-08-11 09:08:13 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,16 @@ import AppCounter from "./Counter/Counter";
 import Painting from "./Paintings/Paitings";
 import CounterSecond from "./CounterSecond/CounterSecond";
 import paintings from "./databaseJSON/paitings.json";
-import httpRequest from "./httpRequest/httpRequest";
+import TaskList from "./TaskList/TaskList";
 import Counter from './CounterHook/Hook'
+
+
 
 export const App = () => {
   return(
     <>
+<TaskList/>
+
     <Counter/>
 
     <httpRequest/>
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,1 +1,39 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
+
+function TaskList () {
+  const [tasks, setTask]=useState([])
+  const taskInputRef = useRef(null);
+
+  useEffect(()=>{
+    const savedTasks = useRef(null);
+  })
+
+  const addTask = (newTask)=>{
+setTask([...tasks, newTask]);
+  };
+  const handleAddTask = (event) => {
+    event.preventDefault();
+    const newTask = event.target.elements.task.value;
+    console.log(newTask)
+if (newTask){
+  addTask(newTask);
+  event.target.task.value = '';
+  }
+  };
+  return (
+    <div>
+      <h2>Список завдань</h2>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleAddTask}>
+        <input type="text" name="task" placeholder="Введіть завдання" />
+        <button type="submit">Додати</button>
+      </form>
+    </div>
+  );
+}
+
+export default TaskList;
